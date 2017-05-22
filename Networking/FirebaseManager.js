9132d723d27370a6c396538fdf74474c1c8288d0
Oliver Plunkett <otplunkett@gmail.com>
2017-05-22 10:51:58 -0400

    put firebase in its own file

---
+++ Networking/FirebaseManager.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHQikqQ6e3q78jI9u5Us-uayAFBuFVTgM',
+  authDomain: 'classical-spelling-bee.firebaseapp.com',
+  databaseURL: 'https://classical-spelling-bee.firebaseio.com',
+  storageBucket: "classical-spelling-bee.appspot.com"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const FirebaseManager = {
+  words: firebaseApp.database().ref().child('mobile').child('words')
+}
+
+export default FirebaseManager;
